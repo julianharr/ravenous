58cfc4ef44fdd03709d59bc4c204ba187dc94870
Julian Harrington <jrayharrington@gmail.com>
2021-04-24 13:48:21 +1000

    add sorting object to search bar, add sort options method to search bar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// Stylesheet
+import './SearchBar.css';
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component {
+  renderSortByOptions() {
+
+  }
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default SearchBar;
